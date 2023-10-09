db8aca2a934cf38d1dbb6c45ddad8b73d3add1b1
agsmgmaster64 <agsmgmaster64@gmail.com>
2023-10-09 17:49:28 -0400

    more animation code

--- include/pokemon_summary_screen.h
+++ include/pokemon_summary_screen.h
@@ -15,7 +15,6 @@ void ShowPokemonSummaryScreen(u8 mode, void *mons, u8 monIndex, u8 maxMonIndex, 
 void ShowSelectMovePokemonSummaryScreen(struct Pokemon *mons, u8 monIndex, u8 maxMonIndex, void (*callback)(void), u16 newMove);
 void ShowPokemonSummaryScreenHandleDeoxys(u8 mode, struct BoxPokemon *mons, u8 monIndex, u8 maxMonIndex, void (*callback)(void));
 u8 GetMoveSlotToReplace(void);
-void SummaryScreen_SetAnimDelayTaskId(u8 taskId);
 
 // The Pokemon Summary Screen can operate in different modes. Certain features,
 // such as move re-ordering, are available in the different modes.
