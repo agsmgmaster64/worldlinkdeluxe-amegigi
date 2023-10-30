dfb96bfd2d1d0fb7c49e411cdaea1c83e3a7ba5f
agsmgmaster64 <agsmgmaster64@gmail.com>
2023-10-30 10:52:19 -0400

    Fascinate fix

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -11468,7 +11468,8 @@ static u32 ChangeStatBuffs(s8 statValue, u32 statId, u32 flags, const u8 *BS_ptr
             return STAT_CHANGE_DIDNT_WORK;
         }
         else if ((battlerHoldEffect == HOLD_EFFECT_CLEAR_AMULET
-              || CanAbilityPreventStatLoss(battlerAbility, GetBattlerAbility(gBattlerAttacker) == ABILITY_INTIMIDATE))
+              || CanAbilityPreventStatLoss(battlerAbility, GetBattlerAbility(gBattlerAttacker) == ABILITY_INTIMIDATE)
+              || CanAbilityPreventStatLoss(battlerAbility, GetBattlerAbility(gBattlerAttacker) == ABILITY_FASCINATE))
               && (!affectsUser || mirrorArmored) && !certain && gBattleMoves[gCurrentMove].effect != EFFECT_CURSE)
         {
             if (flags == STAT_CHANGE_ALLOW_PTR)
