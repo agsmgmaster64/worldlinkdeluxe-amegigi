670adb0c0c86930d61d9ab558bdd3dd6949259f4
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2024-12-31 22:47:20 -0500

    final compiler fixes of the year

--- src/battle_util.c
+++ src/battle_util.c
@@ -12196,7 +12196,7 @@ bool32 DoesDestinyBondFail(u32 battler)
 // This check has always to be the last in a condtion statement because of the recording of AI data.
 bool32 IsMoveEffectBlockedByTarget(u32 ability)
 {
-    if (ability == ABILITY_SHIELD_DUST)
+    if (ability == ABILITY_ADVENT)
     {
         RecordAbilityBattle(gBattlerTarget, ability);
         return TRUE;
