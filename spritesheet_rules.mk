82cd84fa1873d712d6c3089cf1b4221c431079c5
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2025-01-30 20:31:36 -0500

    funni Ina thing

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -429,6 +429,9 @@ $(OBJEVENTGFXDIR)/people/gigi_murin.4bpp: %.4bpp: %.png
 $(OBJEVENTGFXDIR)/people/gym_leaders/larry.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
+$(OBJEVENTGFXDIR)/people/ina_artist.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 2 -mheight 4
+
 
 $(OBJEVENTGFXDIR)/pokemon_old/azumarill.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 2
