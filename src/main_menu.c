d02e574152939c198fa773634074e43a5a5869a1
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2025-02-17 19:51:15 -0500

    mapping stuff

--- src/main_menu.c
+++ src/main_menu.c
@@ -478,8 +478,8 @@ static const union AffineAnimCmd *const sSpriteAffineAnimTable_PlayerShrink[] =
 };
 
 static const struct MenuAction sMenuActions_Gender[] = {
-    {COMPOUND_STRING("BOY"), {NULL}},
-    {COMPOUND_STRING("GIRL"), {NULL}}
+    {COMPOUND_STRING("Tomboy"), {NULL}},
+    {COMPOUND_STRING("Girly"), {NULL}}
 };
 
 static const u8 *const sMalePresetNames[] = {
