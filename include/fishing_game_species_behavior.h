2620fc6012b6876d705f6fad2b676bbdc7c15303
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2025-05-01 22:15:40 -0400

    compiler fixes

--- include/fishing_game_species_behavior.h
+++ include/fishing_game_species_behavior.h
@@ -38,124 +38,115 @@ static const struct FishBehaviorData sFishBehavior[] =
     },
     */
     {
-        .species = SPECIES_TENTACOOL,
-        .speed = { .min = 3, .max = 5 },
-        .distance = { .min = 25, .max = 55 },
-        .delay = { .min = 80, .max = 120 },
+        .species = SPECIES_CHIBI_MURASA,
+        .speed.min = 3,
+        .speed.max = 5,
+        .distance.min = 25,
+        .distance.max = 55,
+        .delay.min = 80,
+        .delay.max = 120,
         .idleMovement = 6
     },
     {
-        .species = SPECIES_TENTACRUEL,
-        .speed = { .min = 8, .max = 10 },
-        .distance = { .min = 25, .max = 75 },
-        .delay = { .min = 80, .max = 120 },
+        .species = SPECIES_NORMAL_MURASA,
+        .speed.min = 8,
+        .speed.max = 10,
+        .distance.min = 25,
+        .distance.max = 75,
+        .delay.min = 80,
+        .delay.max = 120,
         .idleMovement = 8
     },
     {
-        .species = SPECIES_HORSEA,
-        .speed = { .min = 4, .max = 10 },
-        .distance = { .min = 40, .max = 80 },
-        .delay = { .min = 30, .max = 70 },
-        .idleMovement = 7
-    },
-    {
-        .species = SPECIES_GOLDEEN,
-        .speed = { .min = 4, .max = 10 },
-        .distance = { .min = 60, .max = 80 },
-        .delay = { .min = 170, .max = 190 },
+        .species = SPECIES_CHIBI_WAKASAGIHIME,
+        .speed.min = 4,
+        .speed.max = 10,
+        .distance.min = 60,
+        .distance.max = 80,
+        .delay.min = 170,
+        .delay.max = 190,
         .idleMovement = 4
     },
     {
-        .species = SPECIES_SEAKING,
-        .speed = { .min = 6, .max = 12 },
-        .distance = { .min = 50, .max = 90 },
-        .delay = { .min = 100, .max = 140 },
+        .species = SPECIES_NORMAL_WAKASAGIHIME,
+        .speed.min = 6,
+        .speed.max = 12,
+        .distance.min = 50,
+        .distance.max = 90,
+        .delay.min = 100,
+        .delay.max = 140,
         .idleMovement = 6
     },
     {
-        .species = SPECIES_STARYU,
-        .speed = { .min = 7, .max = 13 },
-        .distance = { .min = 15, .max = 45 },
-        .delay = { .min = 20, .max = 80 },
-        .idleMovement = 5
-    },
-    {
-        .species = SPECIES_MAGIKARP,
-        .speed = { .min = 2, .max = 2 },
-        .distance = { .min = 5, .max = 15 },
-        .delay = { .min = 40, .max = 160 },
+        .species = SPECIES_CHIBI_HINA,
+        .speed.min = 2,
+        .speed.max = 2,
+        .distance.min = 5,
+        .distance.max = 15,
+        .delay.min = 40,
+        .delay.max = 160,
         .idleMovement = 6
     },
     {
-        .species = SPECIES_GYARADOS,
-        .speed = { .min = 8, .max = 16 },
-        .distance = { .min = 30, .max = 70 },
-        .delay = { .min = 15, .max = 35 },
+        .species = SPECIES_NORMAL_HINA,
+        .speed.min = 8,
+        .speed.max = 16,
+        .distance.min = 30,
+        .distance.max = 70,
+        .delay.min = 15,
+        .delay.max = 35,
         .idleMovement = 12
     },
     {
-        .species = SPECIES_CORSOLA,
-        .speed = { .min = 4, .max = 8 },
-        .distance = { .min = 10, .max = 70 },
-        .delay = { .min = 55, .max = 95 },
-        .idleMovement = 1
-    },
-    {
-        .species = SPECIES_CARVANHA,
-        .speed = { .min = 7, .max = 13 },
-        .distance = { .min = 15, .max = 25 },
-        .delay = { .min = 30, .max = 70 },
+        .species = SPECIES_CHIBI_KOMACHI,
+        .speed.min = 7,
+        .speed.max = 13,
+        .distance.min = 15,
+        .distance.max = 25,
+        .delay.min = 30,
+        .delay.max = 70,
         .idleMovement = 15
     },
     {
-        .species = SPECIES_SHARPEDO,
-        .speed = { .min = 10, .max = 30 },
-        .distance = { .min = 55, .max = 95 },
-        .delay = { .min = 70, .max = 90 },
+        .species = SPECIES_NORMAL_KOMACHI,
+        .speed.min = 10,
+        .speed.max = 30,
+        .distance.min = 55,
+        .distance.max = 95,
+        .delay.min = 70,
+        .delay.max = 90,
         .idleMovement = 20
     },
     {
-        .species = SPECIES_WAILMER,
-        .speed = { .min = 6, .max = 8 },
-        .distance = { .min = 60, .max = 140 },
-        .delay = { .min = 10, .max = 20 },
-        .idleMovement = 2
-    },
-    {
-        .species = SPECIES_BARBOACH,
-        .speed = { .min = 4, .max = 8 },
-        .distance = { .min = 10, .max = 40 },
-        .delay = { .min = 45, .max = 75 },
+        .species = SPECIES_CHIBI_NITORI,
+        .speed.min = 4,
+        .speed.max = 8,
+        .distance.min = 10,
+        .distance.max = 40,
+        .delay.min = 45,
+        .delay.max = 75,
         .idleMovement = 6
     },
     {
-        .species = SPECIES_WHISCASH,
-        .speed = { .min = 8, .max = 12 },
-        .distance = { .min = 25, .max = 55 },
-        .delay = { .min = 30, .max = 60 },
+        .species = SPECIES_NORMAL_NITORI,
+        .speed.min = 8,
+        .speed.max = 12,
+        .distance.min = 25,
+        .distance.max = 55,
+        .delay.min = 30,
+        .delay.max = 60,
         .idleMovement = 5
     },
     {
-        .species = SPECIES_CORPHISH,
-        .speed = { .min = 7, .max = 13 },
-        .distance = { .min = 4, .max = 10 },
-        .delay = { .min = 30, .max = 70 },
-        .idleMovement = 8
-    },
-    {
-        .species = SPECIES_FEEBAS,
-        .speed = { .min = 4, .max = 6 },
-        .distance = { .min = 8, .max = 22 },
-        .delay = { .min = 40, .max = 140 },
+        .species = SPECIES_CHIBI_WAKASAGIHIME_GOLD,
+        .speed.min = 4,
+        .speed.max = 6,
+        .distance.min = 8,
+        .distance.max = 22,
+        .delay.min = 40,
+        .delay.max = 140,
         .idleMovement = 6
     },
-    {
-        .species = SPECIES_LUVDISC,
-        .speed = { .min = 6, .max = 8 },
-        .distance = { .min = 35, .max = 65 },
-        .delay = { .min = 10, .max = 40 },
-        .idleMovement = 3
-    }
 };
 
 #endif // GUARD_FISHING_GAME_SPECIES_BEHAVIOR_H
--- include/fishing_game_treasures.h
+++ include/fishing_game_treasures.h
@@ -8,10 +8,10 @@ static const u16 sTreasureItems[] =
 {
     // Common
     ITEM_POTION,
-    ITEM_POKE_BALL,
+    ITEM_TOHO_ORB,
     ITEM_ANTIDOTE,
     ITEM_X_ATTACK,
-    ITEM_X_DEFEND,
+    ITEM_X_DEFENSE,
     ITEM_REPEL,
     ITEM_ESCAPE_ROPE,
     ITEM_POKE_DOLL,
@@ -20,7 +20,7 @@ static const u16 sTreasureItems[] =
 
     // Uncommon
     ITEM_SUPER_POTION,
-    ITEM_GREAT_BALL,
+    ITEM_GREAT_ORB,
     ITEM_REVIVE,
     ITEM_SUPER_REPEL,
     ITEM_ETHER,
@@ -28,19 +28,19 @@ static const u16 sTreasureItems[] =
     ITEM_HP_UP,
     ITEM_MAX_ELIXIR,
     ITEM_NUGGET,
-    ITEM_HEART_SCALE,
+    ITEM_BLUE_UFO,
 
     // Rare
     ITEM_HYPER_POTION,
-    ITEM_ULTRA_BALL,
+    ITEM_ULTRA_ORB,
     ITEM_FULL_RESTORE,
     ITEM_MAX_REPEL,
     ITEM_MAX_REVIVE,
     ITEM_ELIXIR,
     ITEM_RARE_CANDY,
     ITEM_KINGS_ROCK,
-    ITEM_LEFTOVERS,
-    ITEM_TM_EARTHQUAKE,
+    ITEM_BENTO_BOX,
+    ITEM_BIG_NUGGET,
 };
 
 #endif // GUARD_FISHING_GAME_TREASURES_H
