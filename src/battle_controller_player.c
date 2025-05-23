7f62884da0882859447135ab3eadcc251fcf27d5
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2025-05-23 17:37:40 -0400

    partially fix going back from the new menu
    This is still not functional

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -2467,7 +2467,7 @@ static void Controller_WaitForDebug(u32 battler)
 static void PlayerHandleBattleDebug(u32 battler)
 {
     BeginNormalPaletteFade(-1, 0, 0, 0x10, 0);
-    UI_Battle_Menu_Init(BattleMainCB2);
+    UI_Battle_Menu_Init(ReshowBattleScreenAfterMenu);
     gBattlerControllerFuncs[battler] = Controller_WaitForDebug;
 }
 
