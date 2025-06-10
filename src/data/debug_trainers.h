aa1100d601583f5273270654d6d338c5a8983402
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2025-06-10 14:50:04 -0400

    compiler fixes

--- src/data/debug_trainers.h
+++ src/data/debug_trainers.h
@@ -16,7 +16,7 @@
 #line 16
         .trainerClass = TRAINER_CLASS_PKMN_TRAINER_1,
 #line 17
-        .trainerPic = TRAINER_PIC_BRENDAN,
+        .trainerPic = TRAINER_PIC_RENKO,
         .encounterMusic_gender =
 #line 19
             TRAINER_ENCOUNTER_MUSIC_MALE,
@@ -25,9 +25,9 @@
         {
             {
 #line 21
-            .nickname = COMPOUND_STRING("Buffie"),
+            .nickname = COMPOUND_STRING("Spamton"),
 #line 21
-            .species = SPECIES_WOBBUFFET,
+            .species = SPECIES_NORMAL_KRONII,
             .gender = TRAINER_MON_RANDOM_GENDER,
 #line 25
             .ev = TRAINER_PARTY_EVS(0, 252, 252, 0, 6, 0),
@@ -37,7 +37,6 @@
             .lvl = 100,
 #line 22
             .nature = NATURE_BRAVE,
-            .dynamaxLevel = MAX_DYNAMAX_LEVEL,
             .moves = {
 #line 26
                 MOVE_EARTHQUAKE,
@@ -53,54 +52,54 @@
     {
 #line 32
         .trainerName = _("Debugger"),
-#line 33
-        .trainerClass = TRAINER_CLASS_RIVAL,
-#line 35
-        .trainerPic = TRAINER_PIC_STEVEN,
-        .encounterMusic_gender =
-#line 37
-            TRAINER_ENCOUNTER_MUSIC_MALE,
 #line 34
+        .trainerClass = TRAINER_CLASS_RIVAL,
+#line 36
+        .trainerPic = TRAINER_PIC_GIGI_MURIN,
+        .encounterMusic_gender =
+#line 38
+            TRAINER_ENCOUNTER_MUSIC_MALE,
+#line 35
         .battleType = TRAINER_BATTLE_TYPE_SINGLES,
+#line 33
+        .aiFlags = AI_FLAG_BASIC_TRAINER,
         .partySize = 3,
         .party = (const struct TrainerMon[])
         {
             {
-#line 39
-            .species = SPECIES_METANG,
-            .gender = TRAINER_MON_RANDOM_GENDER,
-#line 43
-            .ev = TRAINER_PARTY_EVS(0, 252, 252, 0, 6, 0),
-#line 42
-            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
-#line 41
-            .lvl = 42,
 #line 40
-            .nature = NATURE_BRAVE,
-            .dynamaxLevel = MAX_DYNAMAX_LEVEL,
-            .moves = {
+            .species = SPECIES_NORMAL_NUE,
+            .gender = TRAINER_MON_RANDOM_GENDER,
 #line 44
+            .ev = TRAINER_PARTY_EVS(0, 252, 252, 0, 6, 0),
+#line 43
+            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
+#line 42
+            .lvl = 42,
+#line 41
+            .nature = NATURE_BRAVE,
+            .moves = {
+#line 45
                 MOVE_LIGHT_SCREEN,
-                MOVE_PSYCHIC,
+                MOVE_MANA_BURST,
                 MOVE_REFLECT,
                 MOVE_METAL_CLAW,
             },
             },
             {
-#line 49
-            .species = SPECIES_SKARMORY,
-            .gender = TRAINER_MON_RANDOM_GENDER,
-#line 53
-            .ev = TRAINER_PARTY_EVS(252, 0, 0, 0, 6, 252),
-#line 52
-            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
-#line 51
-            .lvl = 43,
 #line 50
-            .nature = NATURE_IMPISH,
-            .dynamaxLevel = MAX_DYNAMAX_LEVEL,
-            .moves = {
+            .species = SPECIES_NORMAL_MEDICINE,
+            .gender = TRAINER_MON_RANDOM_GENDER,
 #line 54
+            .ev = TRAINER_PARTY_EVS(252, 0, 0, 0, 6, 252),
+#line 53
+            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
+#line 52
+            .lvl = 43,
+#line 51
+            .nature = NATURE_IMPISH,
+            .moves = {
+#line 55
                 MOVE_TOXIC,
                 MOVE_AERIAL_ACE,
                 MOVE_PROTECT,
@@ -108,24 +107,23 @@
             },
             },
             {
-#line 59
-            .species = SPECIES_AGGRON,
-            .gender = TRAINER_MON_RANDOM_GENDER,
-#line 63
-            .ev = TRAINER_PARTY_EVS(0, 252, 0, 0, 252, 6),
-#line 62
-            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
-#line 61
-            .lvl = 44,
 #line 60
-            .nature = NATURE_ADAMANT,
-            .dynamaxLevel = MAX_DYNAMAX_LEVEL,
-            .moves = {
+            .species = SPECIES_NORMAL_AMELIA,
+            .gender = TRAINER_MON_RANDOM_GENDER,
 #line 64
+            .ev = TRAINER_PARTY_EVS(0, 252, 0, 0, 252, 6),
+#line 63
+            .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
+#line 62
+            .lvl = 44,
+#line 61
+            .nature = NATURE_ADAMANT,
+            .moves = {
+#line 65
                 MOVE_THUNDER,
                 MOVE_PROTECT,
                 MOVE_SOLAR_BEAM,
-                MOVE_DRAGON_CLAW,
+                MOVE_HEART_BREAK,
             },
             },
         },
