848789e4d549c9ac307d3866dd6e1ee835ad6aac
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2025-10-07 10:25:13 -0400

    fixes

--- src/pokedex_plus_hgss.c
+++ src/pokedex_plus_hgss.c
@@ -6665,6 +6665,7 @@ static void PrintEvolutionTargetSpeciesAndMethod(u8 taskId, u16 species, u8 dept
                     case REGION_GALAR: StringAppend(gStringVar4, COMPOUND_STRING("Galar")); break;
                     case REGION_HISUI: StringAppend(gStringVar4, COMPOUND_STRING("Hisui")); break;
                     case REGION_PALDEA: StringAppend(gStringVar4, COMPOUND_STRING("Paldea")); break;
+                    case REGION_SANNI: StringAppend(gStringVar4, COMPOUND_STRING("Sanni")); break;
                     }
                     break;
                 }
